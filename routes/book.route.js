ce1eabc3e18e5ef9ca424d5a003ad98c9649c2b5
DES <yadavdes7@gmail.com>
2024-01-24 16:15:34 +0545

    routings

--- routes/book.route.js
+++ routes/book.route.js
@@ -3,4 +3,20 @@ const router = require("express").Router(); //chaining of methods
 router.get("/", (req, res) => {
   res.json({ data: `hello form Books` });
 });
+//req.params=req.params.id//detail page
+//req.query=?key=valuepagination
+//req.body=raw json({"key":"value"})//form
+
+router.post("/", (req, res) => {
+  console.log(req.query, req.body);
+});
+router.put("/", (req, res) => {
+  console.log(req.query, req.body);
+});
+router.patch("/", (req, res) => {
+  console.log(req.query, req.body);
+});
+router.delete("/", (req, res) => {
+  console.log(req.body, req.body);
+});
 module.exports = router;
